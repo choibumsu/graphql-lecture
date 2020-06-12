ee85490a9cc886490f3c27e2a94cd11385ea41e9
choibumsu <bumsu0211@gmail.com>
2020-06-12 21:24:50 +0900

    GraphQL 세션 준비

---
+++ graphql/resolvers.js
@@ -0,0 +1,88 @@
+import * as data from "./db.json";
+
+const programmers = data.programmers;
+const friendships = data.friendships;
+
+const getProgrammers = () => {
+  return programmers.map((programmer) => {
+    programmer.friends = friendships.reduce((friendsList, friendship) => {
+      if (
+        programmer.id === friendship.friend_a ||
+        programmer.id === friendship.friend_b
+      )
+        friendsList.push({
+          id: friendship.id,
+          friend_a: programmers.filter(
+            (programmer) => programmer.id === friendship.friend_a
+          )[0],
+          friend_b: programmers.filter(
+            (programmer) => programmer.id === friendship.friend_b
+          )[0],
+          metYear: friendship.metYear,
+        });
+
+      return friendsList;
+    }, []);
+
+    return programmer;
+  });
+};
+
+const resolvers = {
+  Category: {
+    __resolveType(obj, context, info) {
+      if (obj.database !== undefined) {
+        return "Backend";
+      } else if (obj.native !== undefined) {
+        return "Frontend";
+      } else {
+        return null;
+      }
+    },
+  },
+  Query: {
+    programmers: () => getProgrammers(),
+    programmer: (_, { id }) => data,
+  },
+  Mutation: {
+    addProgrammer: (_, { input }) => {
+      const newProgrammer = {
+        id: programmers.length + 1,
+        name: input.name,
+        gender: input.gender,
+        grades: input.grades,
+        address: {
+          location: input.addressLocation,
+          detail: input.addressDetail ? input.addressDetail : "",
+        },
+        category: {
+          frameworks: input.frameworks,
+        },
+        friendship_ids: [],
+      };
+
+      input.friends.forEach((friendId) => {
+        newProgrammer.friendship_ids.push(friendships.length + 1);
+        friendships.push({
+          id: friendships.length + 1,
+          friend_a: programmers.length + 1,
+          friend_b: friendId,
+          metYear: new Date().getFullYear(),
+        });
+      });
+
+      if (input.category === "Frontend") {
+        newProgrammer.category.native = input.native ? input.native : false;
+      } else if (input.category === "Backend") {
+        newProgrammer.category.database = input.database ? input.database : [];
+      } else {
+        return;
+      }
+
+      programmers.push(newProgrammer);
+      return newProgrammer;
+    },
+  },
+};
+
+export default resolvers;
